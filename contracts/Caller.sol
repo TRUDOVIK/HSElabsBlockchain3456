ba18093095f6b6d872245237652424e87aaf61b6
TRUDOVIK <idfomin1@gmail.com>
2023-06-29 19:37:47 +0300

    first commit

---
+++ contracts/Caller.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+contract Caller {
+
+    function callContract(address addr, bytes32 commitment) public payable {
+        (bool res, ) = addr.call{value: msg.value, gas: 500000}(abi.encodeWithSignature("commit(bytes32)", commitment));
+        require(res == true, "Fail!t");
+    }
+
+    receive() external payable {
+    }
+}
